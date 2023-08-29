194096f62f02d4ac752670c9f071bd633b5240de
saikrishnakovuru <saikrishnakovuru1995@gmail.com>
2023-08-29 17:22:57 -0400

    initial commit

---
+++ src/main/java/com/mongodb/MongodbTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbTestAppApplication.class, args);
+	}
+
+}
